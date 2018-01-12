d1c29bb26c1b61060095425533a66a4a9c207927
Leonard Gram <leo@xlson.com>
2018-01-12 17:13:43 +0100

    WIP: mappings for the RowValue types.

--- pkg/plugins/datasource/tsdb/datasource_plugin_wrapper.go
+++ pkg/plugins/datasource/tsdb/datasource_plugin_wrapper.go
@@ -6,10 +6,18 @@ import (
 	"github.com/grafana/grafana/pkg/tsdb"
 	proto "github.com/grafana/grafana/pkg/tsdb/models"
 	"golang.org/x/net/context"
+	"github.com/grafana/grafana/pkg/log"
+	"fmt"
 )
+
+func NewDatasourcePluginWrapper(log log.Logger, plugin TsdbPlugin) *DatasourcePluginWrapper {
+	return &DatasourcePluginWrapper{TsdbPlugin: plugin, logger: log}
+}
 
 type DatasourcePluginWrapper struct {
 	TsdbPlugin
+
+	logger log.Logger
 }
 
 func (tw *DatasourcePluginWrapper) Query(ctx context.Context, ds *models.DataSource, query *tsdb.TsdbQuery) (*tsdb.Response, error) {
@@ -77,17 +85,68 @@ func (tw *DatasourcePluginWrapper) Query(ctx context.Context, ds *models.DataSou
 				Points: points,
 			})
 		}
-		table := tsdb.Table{}
-		for _, t := range r.GetTables() {
-			rows := tsdb.RowValues{}
-			for _, r := range t.GetRows() {
-				values := []interface{}{}
-				for _, v := r.GetValues() {
 
-				}
-			}
+		mappedTables, err := tw.mapTables(r)
+		if err != nil {
+			return nil, err
 		}
+		res.Results[r.RefId].Tables = mappedTables
 	}
 
 	return res, nil
 }
+func (tw *DatasourcePluginWrapper) mapTables(r *proto.QueryResult) ([]*tsdb.Table, error) {
+	var tables []*tsdb.Table
+	for _, t := range r.GetTables() {
+		mappedTable, err := tw.mapTable(t)
+		if err != nil {
+			return nil, err
+		}
+		tables = append(tables, mappedTable)
+	}
+	return tables, nil
+}
+
+func (tw *DatasourcePluginWrapper) mapTable(t *proto.Table) (*tsdb.Table, error) {
+	table := &tsdb.Table{}
+	for _, c := range t.GetColumns() {
+		table.Columns = append(table.Columns, tsdb.TableColumn{
+			Text: c.Name,
+		})
+	}
+
+	for _, r := range t.GetRows() {
+		row := tsdb.RowValues{}
+		for _, rv := range r.Values {
+			mappedRw, err := tw.mapRowValue(rv)
+			if err != nil {
+				return nil, err
+			}
+
+			row = append(row, mappedRw)
+		}
+		table.Rows = append(table.Rows, row)
+	}
+
+	return table, nil
+}
+func (tw *DatasourcePluginWrapper) mapRowValue(rv *proto.RowValue) (interface{}, error) {
+	switch rv.Kind {
+	case proto.RowValue_TYPE_NULL:
+		return nil, nil
+	case proto.RowValue_TYPE_INT64:
+		return rv.Int64Value, nil
+	case proto.RowValue_TYPE_BOOL:
+		return rv.BoolValue, nil
+	case proto.RowValue_TYPE_STRING:
+		return rv.StringValue, nil
+	case proto.RowValue_TYPE_DOUBLE:
+		return rv.DoubleValue, nil
+	case proto.RowValue_TYPE_BYTES:
+		return rv.BytesValue, nil
+	default:
+		return nil, fmt.Errorf("Unsupported row value %v from plugin", rv.Kind)
+	}
+}
+
+
